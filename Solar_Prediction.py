26ec6ebf20a1796053ebbe80d4ebe6ad4d3f44fd
trentonamasa <36996745+trentonamasa@users.noreply.github.com>
2018-10-09 19:16:52 -0600

    Updated teh architecture and the validation set percentage.

--- Solar_Prediction.py
+++ Solar_Prediction.py
@@ -9,12 +9,17 @@ import math
 import csv
 from datetime import datetime
 from tflearn.data_utils import load_csv, to_categorical
+import matplotlib.pyplot as plt
+import tensorboard
+
+# Graph the stuff in matplotlib.
+
 
 def main():
 	# import the weather data including header row
 	DATA_FILE = 'c:/Users/Trent/Documents/School/201840/CS_5890/PythonStuff/Weather_Prediction/weather.csv'
 	data, labels = load_csv(DATA_FILE, target_column=11, columns_to_ignore=[0])
-	
+
 	TrainingSetFeatures = data
 	TrainingSetFeatures = preProcessData(TrainingSetFeatures)
 	TrainingSetLabels = labels
@@ -30,12 +35,12 @@ def main():
 	net = tflearn.regression(net)
 
 	# Define model
-	model = tflearn.DNN(net)
+	model = tflearn.DNN(net, tensorboard_verbose=3)
 
 	# Start training
-	model.fit(TrainingSetFeatures, TrainingSetLabels, n_epoch=3, batch_size=12, show_metric=True)
-		
-	'''Differnt activations and their accuracy:
+	model.fit(TrainingSetFeatures, TrainingSetLabels, n_epoch = 15, validation_set = 0.15, batch_size=12, show_metric=True)	
+
+	'''Differnt architectures and their average accuracy:
 	Softmax ~ 0.6015
 	Linear ~ 0.1519
 	Sigmoid ~ 0.5926
@@ -70,28 +75,28 @@ def preProcessData(data):
 	return tempData
 
 
-def catagorizeLabels(labels):
+def catagorizeLabels(tempLabels):
 	# this needs to be adjusted....
-	for i in range(len(labels)):
-		evSample = float(labels[i])
+	for i in range(len(tempLabels)):
+		evSample = float(tempLabels[i])
 		if evSample > 4000:
-			labels[i] = 4
+			tempLabels[i] = 4
 		elif evSample > 3500:
-			labels[i] = 3.5
+			tempLabels[i] = 3.5
 		elif evSample > 3000:
-			labels[i] = 3
+			tempLabels[i] = 3
 		elif evSample > 2500:
-			labels[i] = 2.5
+			tempLabels[i] = 2.5
 		elif evSample > 2000:
-			labels[i] = 2
+			tempLabels[i] = 2
 		elif evSample > 1500:
-			labels[i] = 1.5
+			tempLabels[i] = 1.5
 		elif evSample > 1000:
-			labels[i] = 1
+			tempLabels[i] = 1
 		elif evSample > 500:
-			labels[i] = 0.5
+			tempLabels[i] = 0.5
 		else:
-			labels[i] = 0	
+			tempLabels[i] = 0	
 
 
 if __name__ == '__main__':
